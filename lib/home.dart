6d896ad42a7f0e6b8ab2792c5e3fb98d9f6dc08e
VismayaBR <vismayabr@gmail.com>
2024-05-05 20:22:44 +0530

    completed

--- lib/home.dart
+++ lib/home.dart
@@ -28,7 +28,7 @@ class _HomeState extends State<Home> {
           Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Center(
+              Center( 
                   child: AppText(
                       text: 'REVIEW HUB',
                       weight: FontWeight.bold,
