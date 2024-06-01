3802b5d2709ed117e2faaa2ab585551d5bc39713
fathima nishma <niyaanas@fathimas-MacBook-Pro.local>
2024-06-01 17:09:37 +0530

    update main file
    test file added

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
-);
+  );
   runApp(const MyApp());
 }
 
@@ -21,11 +21,11 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       home: Splash(),
+      //testing
     );
   }
 }
